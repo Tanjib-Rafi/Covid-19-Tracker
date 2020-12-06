dfa239e929582601f023b578f3488485b0588838
rafi <rafitanjibulhasan@gmail.com>
2020-12-06 21:00:42 +0600

    Updates

---
+++ precache-manifest.534d3764a8abb8c5d99067cc46e944bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c895ac3e143ea593f92c",
+    "url": "/Covid-19-Tracker/static/css/main.334245e2.chunk.css"
+  },
+  {
+    "revision": "c895ac3e143ea593f92c",
+    "url": "/Covid-19-Tracker/static/js/main.f56b4b8d.chunk.js"
+  },
+  {
+    "revision": "00edef08b5924811140c",
+    "url": "/Covid-19-Tracker/static/js/runtime~main.e54b7f2d.js"
+  },
+  {
+    "revision": "644ba65172988f3b5804",
+    "url": "/Covid-19-Tracker/static/js/2.35f2da7c.chunk.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "dc13a5e9fca46367cd93030d8b6531a8",
+    "url": "/Covid-19-Tracker/index.html"
+  }
+];
